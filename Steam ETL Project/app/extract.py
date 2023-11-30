7f4964b3681b407081df8b373d7bdaf0ac7cd7a4
Stivy1 <estivenaudi@gmail.com>
2023-11-29 20:47:42 -0500

    Se mejora proyecto

--- Steam ETL Project/app/extract.py
+++ Steam ETL Project/app/extract.py
@@ -1,15 +1,106 @@
 import pandas as pd
 import requests
-from services.mysql_conn import mysql_upload
+from models.models import generate_create_table_query, SteamPlayerStats, GameInfo
+import mysql.connector
+import pandas as pd
 
+#Buscar el modelo de la tabla
+def get_model_class(table_name: str):
+    table_to_class = {
+        "SteamPlayerStats": SteamPlayerStats,
+        "GameInfo": GameInfo
+    }
+    return table_to_class.get(table_name)
+
+# Realizar la conexion de la DB
+table_name = "steam_player_stats"
+conn = mysql.connector.connect(host = 'localhost', user = 'Stivy', passwd = 'Stivy01*', db = 'steam', auth_plugin='mysql_native_password')
+cursor = conn.cursor()
+
+cursor.execute(f"SHOW TABLES LIKE '{table_name}'")
+exist_table = cursor.fetchone()
+print(exist_table)
+
+#Extraer la informacion
 steam_request = requests.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=80385AB39B7C5B72CB7F7E24CDF6CF2E&steamid=76561198365092040&format=json")
 print(steam_request)
 game_list = steam_request.json()['response']['games']
 
 df_game = pd.DataFrame(game_list)
-df_game['rtime_last_played'] = pd.to_datetime(df_game['rtime_last_played'], unit = 's')
+df_game['rtime_last_played'] = pd.to_datetime(df_game['rtime_last_played'], unit = 's').astype(str)
 df_game['never_played'] = df_game['playtime_forever'] == 0
 
 print(df_game)
 
-mysql_upload(df_game, "steam_player_stats")
+#Usar el modelo de la tabla para generar una query DDL que cree la tabla en la BD
+model_class = get_model_class("SteamPlayerStats")
+
+print(model_class)
+
+create_table_query, new_table_name = generate_create_table_query(model_class)
+#print(create_table_query)
+cursor.execute(create_table_query)
+
+#Llenar de informacion la tabla ya creada
+
+columns = []
+for field_name, field_type in model_class.__annotations__.items():
+    columns.append(field_name)
+try:
+    for _, row in df_game.iterrows():
+        data_dict = dict(zip(columns, row))
+        
+        query = f"INSERT INTO {new_table_name} ({', '.join(columns)}) VALUES ({', '.join(['%s'] * len(columns))})"
+        
+        values = tuple(data_dict[column] for column in columns)
+        print(query, values)
+        cursor.execute(query, values)
+    conn.commit()
+except Exception as e:
+    print(f"Failed to upload the information {e}")
+
+#Mismo procedimiento para extraer la informacion de los juegos
+game_name = []
+game_id = []
+for i in range(len(df_game)):
+    game_request = requests.get(f"http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={df_game['appid'][i]}&key=80385AB39B7C5B72CB7F7E24CDF6CF2E&steamid=76561198365092040")
+    try:
+        game_name.append(game_request.json()['playerstats']['gameName'])
+        game_id.append(df_game['appid'][i])
+    except:
+        pass
+
+df_game_info = pd.DataFrame ({
+            'game_id': game_id,
+            'game_name': game_name
+        }
+)
+
+df_game_info =  df_game_info[df_game_info['game_name'] != '']
+print(df_game_info)
+
+model_class = get_model_class("GameInfo")
+
+print(model_class)
+
+create_table_query, new_table_name = generate_create_table_query(model_class)
+#print(create_table_query)
+cursor.execute(create_table_query)
+
+# #Llenar de informacion la tabla ya creada
+
+columns = []
+for field_name, field_type in model_class.__annotations__.items():
+    columns.append(field_name)
+try:
+    for _, row in df_game_info.iterrows():
+        data_dict = dict(zip(columns, row))
+        
+        query = f"INSERT INTO {new_table_name} ({', '.join(columns)}) VALUES ({', '.join(['%s'] * len(columns))})"
+        
+        values = tuple(data_dict[column] for column in columns)
+        print(query, values)
+        cursor.execute(query, values)
+    conn.commit()
+except Exception as e:
+    print(f"Failed to upload the information {e}")
--- Steam ETL Project/app/models/models.py
+++ Steam ETL Project/app/models/models.py
@@ -2,14 +2,20 @@ from pydantic import BaseModel
 from typing import List
 import stringcase
 
-class HiredEmployees(BaseModel):
-    id: int
-    name: str
-    datetime: str
-    department_id: int
-    job_id: int
+class SteamPlayerStats(BaseModel):
+    appid: int
+    playtime_forever: int
+    playtime_windows_forever: str
+    playtime_mac_forever: int
+    playtime_linux_forever: int
+    rtime_last_played: str
+    playtime_disconnected: int
+    never_played: str
 
-
+class GameInfo(BaseModel):
+    game_id: int
+    game_name: str
+    
 
 def generate_create_table_query(model_class):
     table_name = stringcase.snakecase(model_class.__name__)
@@ -17,7 +23,9 @@ def generate_create_table_query(model_class):
     columns = []
 
     for field_name, field_type in model_class.__annotations__.items():
-        if field_name == "id" and field_type == int:
+        if field_name == "appid" and field_type == int:
+            columns.append(f"{field_name} INT AUTO_INCREMENT PRIMARY KEY NOT NULL")
+        if field_name == "game_id" and field_type == int:
             columns.append(f"{field_name} INT AUTO_INCREMENT PRIMARY KEY NOT NULL")
         else:
             sql_type = "INT" if field_type == int else "VARCHAR(255) NULL"
